d3e94ef86aafaf2074dcfab8e3e37f0b11f911c7
Florencia Licarzi <florencia.licarzi13@gmail.com>
2024-11-17 22:30:47 -0300

    Print Hashtable Fixed

--- Ejecucion/Ejercicio4.ps1
+++ Ejecucion/Ejercicio4.ps1
@@ -97,14 +97,14 @@ $action = {
     # Crear un archivo para imprimir el contenido del hashtable
     $hashTableLogFile = Join-Path -Path $PathLog -ChildPath "HashTable_$timestamp.txt"
 
-    if ($dupHashTable.Count -eq 0) {
+    if ($diccionario_arch.Count -eq 0) {
         # Si el hashtable está vacío
         "El hashtable de archivos duplicados está vacío." | Out-File -FilePath $hashTableLogFile -Encoding UTF8
     } else {
         # Si el hashtable tiene contenido
         "Contenido del hashtable de archivos duplicados:" | Out-File -FilePath $hashTableLogFile -Encoding UTF8
-        foreach ($key in $dupHashTable.Keys) {
-            $values = $dupHashTable[$key] -join ", "
+        foreach ($key in $diccionario_arch.Keys) {
+            $values = $diccionario_arch[$key] -join ", "
             "Clave: $key - Valores: $values" | Out-File -FilePath $hashTableLogFile -Encoding UTF8 -Append
         }
     }
